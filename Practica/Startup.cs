26c445fc754ae9d4bc746648c148d7551b7d27ad
lvgutierrez02 <71912902+lvgutierrez02@users.noreply.github.com>
2022-05-04 16:47:48 -0500

    hoy

--- Practica/Startup.cs
+++ Practica/Startup.cs
@@ -19,26 +19,26 @@ namespace Practica
 {
     public class Startup
     {
-        //public Startup(IConfigurationBuilder configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        public IConfigurationBuilder Configuration { get; }
-
         public Startup(IConfigurationBuilder configuration)
         {
             Configuration = configuration;
         }
 
-       
+        public IConfigurationBuilder Configuration { get; }
+
+      
+
+        public IConfigurationBuilder GetConfiguration()
+        {
+            return Configuration;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             //Conexión SQl Server
-         /*   var conexion = [];*/ //Cadena de conexión
+            //var conexion = Configuration[]; //Cadena de conexión
             services.AddDbContext<AppDbContext>(option =>
             option.UseSqlServer("ConnectionStrings:conexion_sqlServer"));
 
